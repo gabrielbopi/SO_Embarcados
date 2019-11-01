23bcc90953d15ecf7e7350833225ed3d13a2d346
gabrielbopi <54039983+gabrielbopi@users.noreply.github.com>
2019-11-01 15:26:53 -0300

    Add files via upload

---
+++ 2_PCs/PC3/Codigo/bib_arqs.h
@@ -0,0 +1,3 @@
+int tam_arq_texto(char *nome_arquivo);
+char* le_arq_texto(char *nome_arquivo);
+
---
+++ 2_PCs/PC3/Codigo/terminal_tranca.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gerencia_contas.h"
+#include "gpio_sysfs.h"
+#include <unistd.h>
+#define ARQUIVO_CONTAS	"usuarios.txt"
+#define entrada_usuario argv[2]
+#define entrada_senha argv[3]
+
+int recolheDigital(void);
+int abreTranca(void);
+
+
+int main(int argc, char * argv[]){
+	const int creditos_minimos = 5;
+	struct usuario usuario_corrente;
+	int polegarAutenticado;
+
+	if (argc < 3) {
+		printf("Escolha uma opcao valida.\n");
+		printf("Para utilizar o programa escreva no terminal:\n./terminal_tranca -a <nome do usuario> <senha do usuario>\n");
+		return -1;
+	}
+	if(argv[1][0]=='-'){
+		switch(argv[1][1]){
+			case 'a':
+				usuario_corrente = leUsuario(ARQUIVO_CONTAS,entrada_usuario);
+				//printf("Usuario digitado: %s, senha digitada:%s\n", entrada_usuario, entrada_senha);
+				//printf("ID: %d, usuario: %s, senha:%s, creditos: %d\n", usuario_corrente.id, usuario_corrente.nome, usuario_corrente.senha, usuario_corrente.creditos);
+				if (strcmp(entrada_usuario, usuario_corrente.nome) == 0){
+					if (strcmp(entrada_senha, usuario_corrente.senha) == 0){
+						//FUNÇÃO PARA ABRIR A TRANCA AQUI!
+						if (usuario_corrente.creditos >= creditos_minimos){
+							//printf("Abre-te Sesamo!\n");
+							//
+							polegarAutenticado = recolheDigital()/256;
+							if(polegarAutenticado == 10) {abreTranca();}
+						}
+						else{
+							printf("Não é possivel utilizar o sistema, usuario sem creditos o suficiente. (Pelo menos %d creditos)\n", creditos_minimos);
+
+						}
+					}
+					else{
+							printf("Senha incorreta!\n");
+					}
+				}
+				else{
+					printf("Usuario nao encontrado.\n");
+					}
+			break;
+			
+			defaut:
+				printf("Escolha uma opcao valida.\n");
+		printf("Para utilizar o programa escreva no terminal:\n./terminal_tranca -a <nome do usuario> <senha do usuario>\n");
+
+		}
+	}
+	
+	else {
+		printf("Para utilizar o programa escreva no terminal:\n./terminal_tranca -a <nome do usuario> <senha do usuario>\n");
+	}
+}
+
+int recolheDigital(void){
+	printf("Resposta: %d\n",system("python /home/pi/Desktop/Codigo/fingerprint_enroll.py"));
+	return 0;
+}
+
+int abreTranca(void){
+	int pin=21;
+	if(setGPIO_Out(pin)){
+		printf("Erro ao tentar abrir a porta...(1)\n");
+		return -1;
+	}
+	if (GPIO_Write(pin,1)){
+		printf("Erro ao tentar abrir a porta...(2)\n");
+		return 1;
+		}
+	printf("Tranca abre...\n");
+	sleep(6);
+	if (GPIO_Write(pin,0)){
+		printf("Erro ao tentar desligar a tranca...(1)\n");
+		return 3;
+		}
+	if(unsetGPIO(pin)){
+		printf("Erro ao tentar desligar a tranca...(2)\n");
+		return 2;
+	}
+	return 0;
+}
